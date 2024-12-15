e2775e5455d9eae99623476cc6128b55f6cf26ad
maniaghili <aligholimani@gmail.com>
2024-12-15 16:25:30 +0330

    add new Project

--- src/assets/hooks/getSingleArticle.tsx
+++ src/assets/hooks/getSingleArticle.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "react-query"
 
 export const useSingleArticle = (shortName:string) => {
 
-const {data : article} = useQuery([shortName,'article'],()=>axios.get(`https://academic.liara.run/v1/articles${shortName}`),{
+const {data : article} = useQuery([shortName,'article'],()=>axios.get(`https://academic.liara.run/v1/articles/${shortName}`),{
     staleTime:1000000,
     cacheTime:1000000
   })
