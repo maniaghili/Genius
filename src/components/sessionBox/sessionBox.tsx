3a89042dea107a58623a07d30f72c0b180098ad2
maniaghili <aligholimani@gmail.com>
2024-12-16 07:56:26 +0330

    add new Project

--- src/components/sessionBox/sessionBox.tsx
+++ src/components/sessionBox/sessionBox.tsx
@@ -32,7 +32,7 @@ const res = Object.groupBy(session,dd)
         <p className="font-bold text-[12px] opacity-60 mx-2">{index + 1}</p>
         <span className="font-bold text-[12px] opacity-80">{ses.title}</span>
         </div>
-        <div className="flex gap-2 h-full">
+        <div className="flex gap-2 w-fit h-full">
             <div className="flex items-center opacity-60">
                 <p className="font-bold text-[12px]">{ses.time}</p>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
@@ -41,14 +41,14 @@ const res = Object.groupBy(session,dd)
                 </svg>
             </div>
             {
-              ses.free?<div onClick={()=>{showIziToast('ناموفق','این قسمت هنوز تکمیل نشده است','yellow')}} className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 w-fit flex justify-between h-full hover:text-blue-700 transition-all cursor-pointer items-center px-2">
+              ses.free?<div onClick={()=>{showIziToast('ناموفق','این قسمت هنوز تکمیل نشده است','yellow')}} className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 w-20 flex justify-between h-full hover:text-blue-700 transition-all cursor-pointer items-center px-2">
               <p className="font-bold text-[12px] ">مشاهده</p>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                   <path fill-rule="evenodd" d="M14.78 14.78a.75.75 0 0 1-1.06 0L6.5 7.56v5.69a.75.75 0 0 1-1.5 0v-7.5A.75.75 0 0 1 5.75 5h7.5a.75.75 0 0 1 0 1.5H7.56l7.22 7.22a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd"></path>
               </svg>
           </div>:<div onClick={()=>{
             showSwal({title:'قفل',text:'شما قادر به مشاهده این جلسه نمی باشید',icon:'error'})
-          }} className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 w-fit flex justify-between h-full hover:text-red-700 transition-all cursor-pointer items-center px-2">
+          }} className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 flex justify-between h-full hover:text-red-700 transition-all cursor-pointer items-center px-2 w-12">
               <p className="font-bold text-[12px] ">قفل</p>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                   <path fill-rule="evenodd" d="M14.78 14.78a.75.75 0 0 1-1.06 0L6.5 7.56v5.69a.75.75 0 0 1-1.5 0v-7.5A.75.75 0 0 1 5.75 5h7.5a.75.75 0 0 1 0 1.5H7.56l7.22 7.22a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd"></path>
@@ -76,14 +76,14 @@ const res = Object.groupBy(session,dd)
                 </svg>
             </div>
             {
-              ses.free?<div className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 w-fit flex justify-between h-full hover:text-blue-700 transition-all cursor-pointer items-center px-2">
+              ses.free?<div className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 flex justify-between h-full hover:text-blue-700 transition-all cursor-pointer items-center px-2">
               <p className="font-bold text-[12px] ">مشاهده</p>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                   <path fill-rule="evenodd" d="M14.78 14.78a.75.75 0 0 1-1.06 0L6.5 7.56v5.69a.75.75 0 0 1-1.5 0v-7.5A.75.75 0 0 1 5.75 5h7.5a.75.75 0 0 1 0 1.5H7.56l7.22 7.22a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd"></path>
               </svg>
           </div>:<div onClick={()=>{
             showSwal({title:'قفل',text:'شما قادر به مشاهده این جلسه نمی باشید',icon:'error'})
-          }} className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 w-fit flex justify-between h-full hover:text-red-700 transition-all cursor-pointer items-center px-2">
+          }} className="bg-slate-200 dark:bg-gray-700 rounded-xl opacity-70 w-20 flex justify-center gap-2 h-full hover:text-red-700 transition-all cursor-pointer items-center px-2">
               <p className="font-bold text-[12px] ">قفل</p>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                   <path fill-rule="evenodd" d="M14.78 14.78a.75.75 0 0 1-1.06 0L6.5 7.56v5.69a.75.75 0 0 1-1.5 0v-7.5A.75.75 0 0 1 5.75 5h7.5a.75.75 0 0 1 0 1.5H7.56l7.22 7.22a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd"></path>
